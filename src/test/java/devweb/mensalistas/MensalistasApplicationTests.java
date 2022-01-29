797951d9913e7e12713bba32d08dc481efb25b17
Daniel Chaves <danielchaves@uepg.br>
2022-01-29 12:06:57 -0300

    criacao do projeto

---
+++ src/test/java/devweb/mensalistas/MensalistasApplicationTests.java
@@ -0,0 +1,13 @@
+package devweb.mensalistas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MensalistasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
